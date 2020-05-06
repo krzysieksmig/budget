934647099c56934d9e703f82ab542e3f44f062ba
Krzysiek Ks <krzysieksmig@gmail.com>
2020-05-06 13:59:48 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import theme from "utils/theme";
 
@@ -10,9 +11,22 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div className="App">
-        <Navigation items={[]} />
-      </div>
+
+      <Router>
+        <Navigation
+          items={[
+            { content: "Homepage", to: "/" },
+            { content: "Budget", to: "/budget" },
+          ]}
+        />
+
+        <Switch>
+          <Route exact path="/">
+            Homepage
+          </Route>
+          <Route path="/budget">Buget page</Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
